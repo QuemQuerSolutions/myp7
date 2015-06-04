e66f3358558b90d61c12f2cdb67197e2a6c0ebcd
Bruno Ribeiro <brunohbr123@gmail.com>
2015-06-04 15:47:24 -0300

    commit

--- myp7/src/main/java/com/plataforma/myp7/bo/ProdutoBO.java
+++ myp7/src/main/java/com/plataforma/myp7/bo/ProdutoBO.java
@@ -3,12 +3,20 @@ package com.plataforma.myp7.bo;
 import static com.plataforma.myp7.util.Utils.setCodRetorno;
 import static com.plataforma.myp7.util.Utils.setMsgRetorno;
 
+import java.util.List;
+
 import org.springframework.ui.Model;
 
+import com.plataforma.myp7.dao.ProdutoDAO;
 import com.plataforma.myp7.data.Produto;
 
 public class ProdutoBO {
 
+	private ProdutoDAO produtoDAO;
+	public ProdutoBO(){
+		produtoDAO = new ProdutoDAO();
+	}
+	
 	public Boolean salvar(Produto produto, Model model) {
 		if(this.isInsertValido(produto, model)){
 			setMsgRetorno(model, "Embalagem salva com sucesso");
@@ -20,5 +28,9 @@ public class ProdutoBO {
 
 	public Boolean isInsertValido(Produto produto, Model model) {
 		return new NcmBO().validaNcm(produto.getNcmProduto());
+	}
+	
+	public List<Produto> obterProdutos(Produto produto){
+		return produtoDAO.obterProdutos(produto);	
 	}
 }
---
+++ myp7/src/main/java/com/plataforma/myp7/dao/ProdutoDAO.java
@@ -0,0 +1,39 @@
+package com.plataforma.myp7.dao;
+
+import static com.plataforma.myp7.config.Conexao.getConexao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.plataforma.myp7.data.Produto;
+import com.plataforma.myp7.util.Utils;
+
+public class ProdutoDAO {
+	private SqlSession session;
+	
+	public ProdutoDAO(){
+		this.session = getConexao();
+	}
+	
+	public List<Produto> obterTodos(){
+		return this.session.selectList("obterTodos");
+	}
+	
+	public List<Produto> obterProdutos(Produto produto){
+		this.setLike(produto);
+		List<Produto> lstProduto = this.session.selectList("obterProdutos", produto);
+		this.cleanLike(produto);
+		return lstProduto;
+	}
+	
+	private void setLike(Produto produto){
+		produto.setDesProduto(Utils.toLike(produto.getDesProduto()));
+	}
+	
+	private void cleanLike(Produto produto){
+		produto.setDesProduto(Utils.cleanLike(produto.getDesProduto()));
+		
+	}
+	
+}
