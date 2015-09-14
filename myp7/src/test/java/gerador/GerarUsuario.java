b4fed27d89b25bb51e61aeea989be9c0b25ce58e
Alvaro <Alvaro@Alvaro-PC>
2015-09-13 23:15:36 -0300

    Alteração no login e nos imports

---
+++ myp7/src/test/java/gerador/GerarUsuario.java
@@ -0,0 +1,22 @@
+package gerador;
+
+import com.plataforma.myp7.bo.CriptografarBO;
+
+public class GerarUsuario {
+	private static String usuario = "'teste@gmail.com'";
+	private static String senha = "abc";
+	private static String tipoUsuario = "R";
+	
+	public static void main(String[] args) {
+
+		StringBuilder out = new StringBuilder();
+		out.append("INSERT INTO USU_USUARIO VALUES (");
+		out.append("2, 'teste', ");
+		out.append(usuario);
+		out.append(", 123123, 'A','");
+		out.append(CriptografarBO.criptografar(senha));
+		out.append("', null, '"+tipoUsuario+"');");
+		
+		System.out.println(out.toString());
+	}
+}
